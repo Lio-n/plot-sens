f530c6eca39bdef66964b9139faf79193cbda403
Lio-n <leonardofontan14@gmail.com>
2025-07-20 20:21:33 -0300

    chore(android): add location permissions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,80 @@
-import Mapbox from '@rnmapbox/maps';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-Mapbox.setAccessToken('sk.eyJ1IjoibGlvLW4iLCJhIjoiY21kYnJ6b3ZqMDNzdzJsb2liOGg1Y2VibiJ9.TCAfwIUmASphf7cDS-yi9Q');
+import Mapbox from "@rnmapbox/maps";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+Mapbox.setAccessToken(
+  "sk.eyJ1IjoibGlvLW4iLCJhIjoiY21kYnJ6b3ZqMDNzdzJsb2liOGg1Y2VibiJ9.TCAfwIUmASphf7cDS-yi9Q"
+);
+const ARGENTINA_COORS = [-38.416097, -63.616672];
 
 const App = () => {
+  const [userLocation, setUserLocation] = useState([0, 0]);
+  const [currLoc, setCurrLoc] = useState(ARGENTINA_COORS);
+  const gotToMyLocation = (loc: number[]) => {
+    console.log("gotToMyLocation actual:", loc);
+
+    setCurrLoc(loc);
+  };
+
   return (
     <View style={styles.page}>
+      <Text style={{ color: "black", fontSize: 24, marginTop: 32 }}>
+        lat : {userLocation[0]} - lon : {userLocation[1]}
+      </Text>
+      <Button
+        onPress={() => gotToMyLocation(userLocation)}
+        title="Go to my location"
+        color="#841584"
+      />
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} logoEnabled={false} attributionEnabled={false}/>
+        <Mapbox.MapView
+          projection="globe"
+          style={styles.map}
+          logoEnabled={false}
+          attributionEnabled={false}
+        >
+          <Mapbox.UserLocation onUpdate={(location)=>{
+            console.log("UserLocation actual:", location);
+            if(location.coords.altitude)setUserLocation([location.coords.altitude,location.coords.longitude])}} />
+          <Mapbox.Camera
+            followZoomLevel={16}
+            followUserLocation
+            animationMode={"flyTo"}
+            centerCoordinate={currLoc}
+          />
+          {/* <Mapbox.PointAnnotation
+            draggable={true}
+            id="asd"
+            onDragEnd={(feature) => {
+              console.log(feature);
+            }}
+            coordinate={[-32.522779, -55.765835]}
+          >
+            <View
+              style={{
+                height: 30,
+                width: 30,
+                backgroundColor: "#cc0000ff",
+                borderRadius: 50,
+                borderColor: "#fff",
+                borderWidth: 3,
+              }}
+            />
+          </Mapbox.PointAnnotation> */}
+        </Mapbox.MapView>
       </View>
     </View>
   );
-}
+};
 
 export default App;
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-  },
+  page: {},
   container: {
+    marginTop: 100,
   },
   map: {
-    width:"100%",
-    height:"100%"
-  }
+    width: "100%",
+    height: "100%",
+  },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,51 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+import { useEffect, useState } from 'react';
+import { PermissionsAndroid } from 'react-native';
+
+
 export default function RootLayout() {
+    const [hasLocationPermissions,sethasLocationPermissions] = useState(false)
+
+  const requestLocationPermission = async  () => 
+  {
+    try {
+const granted = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+    ]);
+
+  /*
+  ACCESS_COARSE_LOCATION:
+    - Access the device’s location at a coarse level of accuracy, typically based on cell tower or WiFi network information.
+  ACCESS_FINE_LOCATION : 
+    - This permission allows an app to access the device’s location at a fine level of accuracy, using GPS or other satellite-based positioning methods
+    - More acurrent than ACCESS_COARSE_LOCATION.
+  */
+
+    const hasLocationPermission =
+      granted['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED;
+
+      if (hasLocationPermission) {
+        sethasLocationPermissions(true)
+        console.log("You can use the location")
+        alert("You can use the location");
+      } else {
+        console.log("location permission denied")
+        alert("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  useEffect(()=>{
+
+      requestLocationPermission()
+
+  },[])
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
